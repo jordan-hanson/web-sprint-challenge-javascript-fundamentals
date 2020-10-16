a3cd3a92b1b954deea1b6d7a6e11a1aaf5898b1e
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-10-16 09:58:45 -0600

    task 3 complete with filter

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -53,6 +53,11 @@ The zoos are concerned about animals with a lower population count. Using filter
 // const lowPopulationAnimals
 // console.log(lowPopulationAnimals);
 
+let lowPopulationAnimals = zooAnimals.filter((mammal) => {
+  return mammal.population < 5;
+})
+console.log(lowPopulationAnimals)
+
 /* Request 4: .reduce()
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
